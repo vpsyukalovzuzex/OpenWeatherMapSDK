5916d8b9c656162a88ba2d81ed46cda08cbcb1d2
Vladimir Psyukalov <vpsyukalov@zuzex.com>
2021-05-14 10:56:16 +0300

    Add keys

--- Sources/OpenWeatherMapSDK/OpenWeatherMapSDK.swift
+++ Sources/OpenWeatherMapSDK/OpenWeatherMapSDK.swift
@@ -93,7 +93,8 @@ public class RequestBuilder {
     
     internal var parameters: [String: String]
     
-    internal var error: Error?
+    internal var invalidFunctionError: Error?
+    internal var missingFunctionError: Error?
     
     public init(_ method: Method) {
         self.method = method
@@ -106,69 +107,65 @@ public class RequestBuilder {
         case .standard:
             break
         default:
-            parameters["units"] = units.rawValue
+            parameters[Keys.units.rawValue] = units.rawValue
         }
         return self
     }
     
     @discardableResult
     public func language(_ language: Language) -> Self {
-        parameters["lang"] = language.rawValue
+        parameters[Keys.lang.rawValue] = language.rawValue
         return self
     }
     
     @discardableResult
     public func city(name: String, stateCode: String? = nil, countryCode: String? = nil) -> Self {
-        check(#function)
         let result = [name, stateCode, countryCode].compactMap { $0 }.joined(separator: ",")
-        parameters["q"] = result
+        parameters[Keys.q.rawValue] = result
         return self
     }
     
     @discardableResult
     public func id(_ id: String) -> Self {
-        check(#function)
-        parameters["id"] = id
+        parameters[Keys.id.rawValue] = id
         return self
     }
     
     @discardableResult
     public func coordinates(lat: Float, lon: Float) -> Self {
-        check(#function)
-        parameters["lat"] = String(lat)
-        parameters["lon"] = String(lon)
+        parameters[Keys.lat.rawValue] = String(lat)
+        parameters[Keys.lon.rawValue] = String(lon)
         return self
     }
     
     @discardableResult
     public func zip(_ zip: String) -> Self {
-        check(#function)
-        parameters["zip"] = zip
+        parameters[Keys.zip.rawValue] = zip
         return self
     }
     
     @discardableResult
     public func rectangle(lonLeft: Float, latBottom: Float, lonRight: Float, latTop: Float, zoom: Int = 10) -> Self {
-        if method != .currentWeatherByRectangle {
-            error = OWMError.invalidFunction(function: #function, method: "\(method)", underlyingError: error)
-        }
-        parameters["bbox"] = "\(lonLeft),\(latBottom),\(lonRight),\(latTop),\(zoom)"
+        parameters[Keys.bbox.rawValue] = "\(lonLeft),\(latBottom),\(lonRight),\(latTop),\(zoom)"
         return self
     }
     
     @discardableResult
     public func circle(_ number: Int) -> Self {
-        if method != .currentWeatherByCircle {
-            error = OWMError.invalidFunction(function: #function, method: "\(method)", underlyingError: error)
-        }
         let cnt = number <= 0 ? 1 : (number > 50 ? 50 : number)
-        parameters["cnt"] = String(cnt)
+        parameters[Keys.cnt.rawValue] = String(cnt)
         return self
     }
     
     @discardableResult
     public func request<T: Decodable>(_ type: T.Type, _ block: @escaping (Result<T, Error>) -> Void) -> URLSessionTask? {
-        if let error = error {
+        checkInvalidFunctions()
+        if let error = invalidFunctionError {
+            block(.failure(error))
+            return nil
+        }
+        checkMissingFunctions()
+        if let error = missingFunctionError {
             block(.failure(error))
             return nil
         }
@@ -193,57 +190,124 @@ public class RequestBuilder {
         }.task
     }
     
+    // MARK: - Private func
+    
     private func buildUrl() -> URL? {
-        parameters["appid"] = OpenWeatherMapSDK.apiKey
+        parameters[Keys.appid.rawValue] = OpenWeatherMapSDK.apiKey
         let parametersString = parameters.map { $0 + "=" + $1 }.joined(separator: "&")
         let urlString = Constants.baseUrl + method.rawValue + "?" + parametersString
         return URL(string: urlString)
     }
     
-    private func check(_ function: String) {
-        let methods: [Method] = [
-            .currentWeatherByRectangle,
-            .currentWeatherByCircle
-        ]
-        if methods.contains(method) {
-            error = OWMError.invalidFunction(function: function, method: "\(method)", underlyingError: error)
+    private func checkInvalidFunctions() {
+        let set = Set(parameters.keys)
+        var keys: [Keys]
+        switch method {
+        case .currentWeather:
+            keys = [
+                .bbox,
+                .cnt
+            ]
+        case .currentWeatherByRectangle:
+            keys = [
+                .q,
+                .id,
+                .lat,
+                .zip,
+                .cnt,
+            ]
+        case .currentWeatherByCircle:
+            keys = [
+                .q,
+                .id,
+                .lat,
+                .zip,
+                .bbox,
+            ]
+        }
+        for key in keys {
+            if set.contains(key.rawValue) {
+                invalidFunctionError = OWMError.invalidFunction(
+                    function: key.functionName,
+                    method: "\(method)",
+                    underlyingError: invalidFunctionError
+                )
+            }
+        }
+    }
+    
+    private func checkMissingFunctions() {
+        let set = Set(parameters.keys)
+        var keys: [Keys] = []
+        switch method {
+        case .currentWeather:
+            let isValid =
+                set.contains(Keys.q.rawValue)   ||
+                set.contains(Keys.id.rawValue)  ||
+                set.contains(Keys.lat.rawValue) ||
+                set.contains(Keys.zip.rawValue)
+            if !isValid {
+                missingFunctionError = OWMError.missingFunction(
+                    function: Keys.q.functionName,
+                    method: "\(method)",
+                    underlyingError: missingFunctionError
+                )
+            }
+        case .currentWeatherByRectangle:
+            keys = [
+                .bbox
+            ]
+        case .currentWeatherByCircle:
+            keys = [
+                .cnt,
+                .lat
+            ]
+        }
+        for key in keys {
+            if !set.contains(key.rawValue) {
+                invalidFunctionError = OWMError.invalidFunction(
+                    function: key.functionName,
+                    method: "\(method)",
+                    underlyingError: invalidFunctionError
+                )
+            }
         }
     }
     
     private func check<T: Decodable>(_ type: T.Type) -> Error? {
-        var error: Error?
-        let typeString = String(describing: type)
-        let methodString = "\(method)"
+        var result: Error?
+        let invalidTypeError = OWMError.invalidType(type: String(describing: type), method: "\(method)")
         switch method {
         case .currentWeather:
             if type != CurrentWeather.self {
-                error = OWMError.invalidType(type: typeString, method: methodString)
+                result = invalidTypeError
             }
         case .currentWeatherByRectangle:
             if type != CurrentWeatherByRectangle.self {
-                error = OWMError.invalidType(type: typeString, method: methodString)
+                result = invalidTypeError
             }
         case .currentWeatherByCircle:
             if type != CurrentWeatherByCircle.self {
-                error = OWMError.invalidType(type: typeString, method: methodString)
+                result = invalidTypeError
             }
         }
-        return error
+        return result
     }
 }
 
-enum OWMError: CustomNSError, LocalizedError {
+public enum OWMError: CustomNSError, LocalizedError {
     
     case urlIsWrong
     case valueIsNil
     case invalidFunction(function: String, method: String, underlyingError: Error?)
     case invalidType(type: String, method: String)
+    case missingFunction(function: String, method: String, underlyingError: Error?)
     
-    static var errorDomain: String {
+    public static var errorDomain: String {
         return "OpenWeatherMapErrorDomain"
     }
     
-    var errorCode: Int {
+    public var errorCode: Int {
         switch self {
         case .urlIsWrong:
             return -100
@@ -253,21 +317,27 @@ enum OWMError: CustomNSError, LocalizedError {
             return -300
         case .invalidType:
             return -301
+        case .missingFunction:
+            return -302
         }
     }
     
-    var errorUserInfo: [String : Any] {
+    public var errorUserInfo: [String : Any] {
         var result = [String: Any]()
+        var resultUnderlyingError: Error?
         switch self {
         case .invalidFunction(_, _, let underlyingError):
-            result[NSUnderlyingErrorKey] = underlyingError
+            resultUnderlyingError = underlyingError
+        case .missingFunction(_, _, let underlyingError):
+            resultUnderlyingError = underlyingError
         default:
             break
         }
+        result[NSUnderlyingErrorKey] = resultUnderlyingError
         return result
     }
     
-    var errorDescription: String? {
+    public var errorDescription: String? {
         switch self {
         case .urlIsWrong:
             return "URL is wrong"
@@ -277,6 +347,41 @@ enum OWMError: CustomNSError, LocalizedError {
             return "Invalid function '\(function)' for '\(method)' method"
         case .invalidType(let type, let method):
             return "Invalid type '\(type)' for '\(method)' method"
+        case .missingFunction(let function, let method, _):
+            return "Missing function '\(function)' for '\(method)' method"
+        }
+    }
+}
+
+internal enum Keys: String {
+    
+    case units = "units"
+    case lang  = "lang"
+    case q     = "q"
+    case id    = "id"
+    case lat   = "lat"
+    case lon   = "lon"
+    case zip   = "zip"
+    case bbox  = "bbox"
+    case cnt   = "cnt"
+    case appid = "appid"
+    
+    var functionName: String {
+        switch self {
+        case .q:
+            return "city(name:stateCode:countryCode:)"
+        case .id:
+            return "id(_:)"
+        case .lat, .lon:
+            return "coordinates(lat:lon:)"
+        case .zip:
+            return "zip(_:)"
+        case .bbox:
+            return "rectangle(lonLeft:latBottom:lonRight:latTop:zoom:)"
+        case .cnt:
+            return "circle(_:)"
+        default:
+            return ""
         }
     }
 }
